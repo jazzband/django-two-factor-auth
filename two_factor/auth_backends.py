672ca611cf733a2e56cbb04cb7246b322419ca57
Bouke Haarsma <bouke@webatoom.nl>
2012-09-10 12:27:06 +0200

    Authentication backends inherit from ModelBackend

--- two_factor/auth_backends.py
+++ two_factor/auth_backends.py
@@ -1,26 +1,16 @@
 # coding=utf-8
-from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
+from django.utils.timezone import now
 from oath import accept_totp
 
-class TokenBackend(object):
+
+class TokenBackend(ModelBackend):
     def authenticate(self, user, token):
         accepted, drift = accept_totp(token, user.secret.seed)
         return user if accepted else None
 
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
 
-
-class VerifiedComputerBackend(object):
+class VerifiedComputerBackend(ModelBackend):
     def authenticate(self, user, computer_id):
-
-    def get_user(self, user_id):
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
         verification = user.verifiedcomputer_set.get(pk=computer_id)
         return user if verification.verified_until > now() else None
