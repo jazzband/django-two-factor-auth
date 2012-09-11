e5557d757b65ead37a02c79faefe2265405474a9
Bouke Haarsma <bouke@webatoom.nl>
2012-09-11 15:35:49 +0200

    Prepared package for packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+# coding=utf8
+from distutils.core import setup
+
+setup(
+    name='django-two-factor-auth',
+    version='0.1.0',
+    author='Bouke Haarsma',
+    author_email='bouke@webatoom.nl',
+    packages=[
+        'two_factor',
+    ],
+    url='http://github.com/Bouke/django-two-factor-auth',
+    description='Complete Two-Factor Authentication for Django',
+    license='MIT',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'oath >= 1.0'
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+)
