f65cbc9c03a289a33677c5201b59d75f375a4e80
Bouke Haarsma <bouke@webatoom.nl>
2012-09-12 11:08:18 +0200

    Simplified enable/disable urls

--- two_factor/urls.py
+++ two_factor/urls.py
@@ -4,7 +4,7 @@ from two_factor import views
 urlpatterns = patterns('two_factor.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^verify/$', 'verify_computer', name='verify'),
-    url(r'^verify/enable/$', views.Enable.as_view(), name='enable'),
-    url(r'^verify/disable/$', views.Disable.as_view(), name='disable'),
+    url(r'^enable/$', views.Enable.as_view(), name='enable'),
+    url(r'^disable/$', views.Disable.as_view(), name='disable'),
     url(r'^twilio_call_app/$', 'twilio_call_app', name='twilio_call_app'),
 )
