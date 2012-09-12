afbba90cefc89f7b717f0d9d51c0f9f367076ddf
Bouke Haarsma <bouke@webatoom.nl>
2012-09-12 11:27:48 +0200

    Auto-detect packages to include

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 # coding=utf8
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='django-two-factor-auth',
     version='0.1.0',
     author='Bouke Haarsma',
     author_email='bouke@webatoom.nl',
-    packages=[
-        'two_factor',
-    ],
+    packages=find_packages(exclude='demo'),
     package_data={'two_factor': ['templates/two_factor/*.html',],},
     url='http://github.com/Bouke/django-two-factor-auth',
     description='Complete Two-Factor Authentication for Django',
