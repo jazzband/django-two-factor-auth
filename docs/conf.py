835051f64c82f35bb34a1d7930307437cb04296e
Bouke Haarsma <bouke@haarsma.eu>
2015-01-10 08:32:12 +0100

    Released 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ copyright = '2014, Bouke Haarsma'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '1.1.0'
+release = '1.1.1'
 
 # The short X.Y version.
 version = '.'.join(release.split('.')[0:2])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-two-factor-auth',
-    version='1.1.0',
+    version='1.1.1',
     description='Complete Two-Factor Authentication for Django',
     long_description=open('README.rst').read(),
     author='Bouke Haarsma',
