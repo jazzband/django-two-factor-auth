7c1797f20eeb676b7b284495be8621efec45b907
Bouke Haarsma <bouke@haarsma.eu>
2015-01-10 08:11:08 +0100

    Fixed #80 -- Don't use anonymous functions in model fields

---
+++ two_factor/migrations/0002_auto_20150110_0810.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django_otp.util
+import two_factor.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('two_factor', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='phonedevice',
+            name='key',
+            field=models.CharField(default=django_otp.util.random_hex, help_text=b'Hex-encoded secret key', max_length=40, validators=[two_factor.models.key_validator]),
+            preserve_default=True,
+        ),
+    ]
