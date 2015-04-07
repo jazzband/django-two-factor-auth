760489c018b056d7ead1a1310cf121ea124e0ce2
Bouke Haarsma <bouke@haarsma.eu>
2015-04-07 20:53:35 +0200

    Mark 1.2 dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-two-factor-auth',
-    version='1.1.1',
+    version='1.2.0-dev',
     description='Complete Two-Factor Authentication for Django',
     long_description=open('README.rst').read(),
     author='Bouke Haarsma',
