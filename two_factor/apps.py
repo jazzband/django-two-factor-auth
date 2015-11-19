a054a2172e4cd7395ebb27a8a20117f95abb3fd0
Bouke Haarsma <bouke@haarsma.eu>
2015-11-19 21:43:53 +0100

    Removed print-debug left overs

--- two_factor/apps.py
+++ two_factor/apps.py
@@ -9,7 +9,5 @@ class TwoFactorConfig(AppConfig):
     verbose_name = "Django Two Factor Authentication"
 
     def ready(self):
-        print("This does also printy")
         if getattr(settings, 'TWO_FACTOR_PATCH_ADMIN', True):
-            print("This does no printy")
             patch_admin()
