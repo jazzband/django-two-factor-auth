54342d9fd56052effffe799cfd1a623767cdfb1c
Bouke Haarsma <bouke@haarsma.eu>
2015-12-08 23:18:49 +0100

    Dropped support for Django 1.7

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setup(
     license='MIT',
     packages=find_packages(exclude=('example', 'tests')),
     install_requires=[
-        'Django>=1.7,<1.9.99',
-        'django_otp>=0.2.0,<0.99',
+        'Django>=1.8,<1.9.99',
+        'django_otp>=0.3.3,<0.99',
         'qrcode>=4.0.0,<4.99',
         'phonenumbers>=7.0.9,<7.99',
         'django-phonenumber-field>=0.7.2,<0.99',
+        'django-formtools',
     ],
     extras_require={
         'Call': ['twilio'],
