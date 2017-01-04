04e27a3ae84c84e4c719264438dd3cd2b0719688
Bouke Haarsma <bouke@haarsma.eu>
2017-01-04 12:06:22 +0100

    LOGOUT_REDIRECT_URL might be set to None

--- two_factor/views/core.py
+++ two_factor/views/core.py
@@ -182,9 +182,9 @@ class LoginView(IdempotentSessionWizardView):
             except StaticDevice.DoesNotExist:
                 context['backup_tokens'] = 0
 
-        if hasattr(settings, 'LOGOUT_REDIRECT_URL'):
+        if getattr(settings, 'LOGOUT_REDIRECT_URL', None):
             context['cancel_url'] = resolve_url(settings.LOGOUT_REDIRECT_URL)
-        elif hasattr(settings, 'LOGOUT_URL'):
+        elif getattr(settings, 'LOGOUT_URL', None):
             warnings.warn(
                 "LOGOUT_URL has been replaced by LOGOUT_REDIRECT_URL, please "
                 "review the URL and update your settings.",
