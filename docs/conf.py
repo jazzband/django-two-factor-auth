5b671df9604fb18cd89749334f4593068cc7a1e1
Bouke Haarsma <bouke@haarsma.eu>
2017-04-05 14:32:41 +0200

    Bump version: 1.5.0 → 1.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ copyright = '2014, Bouke Haarsma'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '1.5.0'
+release = '1.6.0'
 
 # The short X.Y version.
 version = '.'.join(release.split('.')[0:2])
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-two-factor-auth',
-    version='1.5.0',
+    version='1.6.0',
     description='Complete Two-Factor Authentication for Django',
     long_description=open('README.rst').read(),
     author='Bouke Haarsma',
