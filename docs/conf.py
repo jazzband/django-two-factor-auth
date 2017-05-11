74a1e892c4779fe1f965e9676dc8838bd208319f
Bouke Haarsma <bouke@haarsma.eu>
2017-05-11 20:45:03 +0200

    Bump version: 1.6.0 → 1.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ copyright = '2014, Bouke Haarsma'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '1.6.0'
+release = '1.6.1'
 
 # The short X.Y version.
 version = '.'.join(release.split('.')[0:2])
