077a5196d7d49ef88875e9ec3afcf203bad06d15
Fabien Schwob <jibaku@users.noreply.github.com>
2020-03-25 11:42:18 +0100

    Avoid a django_otp version > 0.7.5
    related to the use of random_hex_str (https://github.com/Bouke/django-two-factor-auth/blob/master/two_factor/models.py#L57)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=('example', 'tests')),
     install_requires=[
         'Django>=1.11',
-        'django_otp>=0.6.0,<0.99',
+        'django_otp>=0.6.0,<=0.7.5',
         'qrcode>=4.0.0,<6.99',
         'django-phonenumber-field>=1.1.0,<3.99',
         'django-formtools',
