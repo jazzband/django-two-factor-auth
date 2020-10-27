c1af5a0c339148de96b1c6e3bd34d085fad07abb
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2020-10-27 14:19:24 +0100

    Redirect to setup if 2fa not enabled yet

--- two_factor/admin.py
+++ two_factor/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from django.contrib.admin import AdminSite
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.views import redirect_to_login
+from django.http import HttpResponseRedirect
 from django.shortcuts import resolve_url
+from django.urls import reverse
 from django.utils.http import is_safe_url
 
 from .models import PhoneDevice
@@ -16,6 +18,9 @@ class AdminSiteOTPRequiredMixin(object):
 
     Custom admin views should either be wrapped using :meth:`admin_view` or
     use :meth:`has_permission` in order to secure those views.
+
+    If user has admin permissions but 2FA not setup, then redirect to
+    2FA setup page.
     """
 
     def has_permission(self, request):
@@ -31,7 +36,18 @@ class AdminSiteOTPRequiredMixin(object):
         """
         Redirects to the site login page for the given HttpRequest.
         """
+                 # redirect to admin page after login
         redirect_to = request.POST.get(REDIRECT_FIELD_NAME, request.GET.get(REDIRECT_FIELD_NAME))
+
+        # if user (is_active and is_staff)
+        if request.method == "GET" and super().has_permission(request):
+            # if user has 2FA setup, go to admin homepage
+            if request.user.is_verified():
+                index_path = reverse("admin:index", current_app=self.name)
+            # 2FA not setup. redirect to 2FA setup page
+            else:
+                index_path = reverse("two_factor:setup", current_app=self.name)
+            return HttpResponseRedirect(index_path)
 
         if not redirect_to or not is_safe_url(url=redirect_to, allowed_hosts=[request.get_host()]):
             redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
