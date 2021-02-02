a67371445acd4f4cf023d7d3c73c9e2d0850da52
Javier Paniagua <jpaniagualaconich@gmail.com>
2021-02-02 16:58:44 +0100

    Bump version: 1.14.dev1 → 1.14.dev2

--- docs/conf.py
+++ docs/conf.py
@@ -58,7 +58,7 @@ copyright = '2014, Bouke Haarsma'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '1.14.dev1'
+release = '1.14.dev2'
 
 # The short X.Y version.
 version = '.'.join(release.split('.')[0:2])
