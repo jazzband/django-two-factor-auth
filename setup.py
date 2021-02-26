91ec17f3debca0bb4d54b6e7ee012b0a8dcf687c
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-02-26 16:30:37 +0100

    Updated the packages so they work with 2.2 and up

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(
     license='MIT',
     packages=find_packages(exclude=('example', 'tests')),
     install_requires=[
-        'Django>=1.11',
-        'django_otp>=0.8.0',
+        'Django>=2.2.0,<4.0.0',
+        'django_otp>=1.0.0.<2.0.0',
         'qrcode>=4.0.0,<6.99',
-        'django-phonenumber-field>=1.1.0,<3.99',
+        'django-phonenumber-field>=5.0.0,<6.0.0',
         'django-formtools',
     ],
     extras_require={
