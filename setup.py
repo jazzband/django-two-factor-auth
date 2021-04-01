1b241684893f63101da2ba8d4fe54b78efbb152d
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2021-04-01 19:27:12 +0100

    Bump version: 1.13 → 1.13.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-two-factor-auth',
-    version='1.13',
+    version='1.13.1',
     description='Complete Two-Factor Authentication for Django',
     long_description=open('README.rst', encoding='utf-8').read(),
     author='Bouke Haarsma',
