eca4be95f29da09e6850d574b6a842212ccaf412
Matt Baker <mattheosb@gmail.com>
2022-01-03 19:38:27 +0100

    Fixed flake8 errors

--- tests/test_totpdeviceform.py
+++ tests/test_totpdeviceform.py
@@ -1,12 +1,7 @@
-import time
 from binascii import unhexlify
 
 import django_otp.oath
-from django import forms
-from django.conf import settings
-from django.shortcuts import resolve_url
 from django.test import TestCase
-from django.urls import reverse
 
 from two_factor.forms import TOTPDeviceForm
 
@@ -16,6 +11,7 @@ from .utils import UserMixin
 # as 3 Jan 2022
 TEST_TIME = 1641194517
 
+
 # This class test how the TOTPDeviceForm validator handles drift between its clock
 # and the TOTP device.  If there is a drift in the range [-tolerance, +tolerance],
 # where tolerance is hardcoded to 1, then the TOTP should be accepted.  Outside this
@@ -24,13 +20,14 @@ class TOTPDeviceFormTest(UserMixin, TestCase):
     key = '12345678901234567890'
 
     def fix_time_for_totp(self):
-    	# Override constructor for TOTP class to set a fixed time.  This will
-    	# prevent it from calling time(), which would make the unittest unpredictable
+        # Override constructor for TOTP class to set a fixed time.  This will
+        # prevent it from calling time(), which would make the unittest unpredictable
         old_TOTP_init = django_otp.oath.TOTP.__init__
         self.old_TOTP_init = old_TOTP_init
+
         def new_TOTP_init(self, key, step=30, t0=0, digits=6, drift=0):
-        	old_TOTP_init(self, key, step, t0, digits, drift)
-        	self.time = TEST_TIME
+            old_TOTP_init(self, key, step, t0, digits, drift)
+            self.time = TEST_TIME
         django_otp.oath.TOTP.__init__ = new_TOTP_init
 
     def restore_time_for_totp(self):
@@ -47,35 +44,35 @@ class TOTPDeviceFormTest(UserMixin, TestCase):
         self.restore_time_for_totp()
 
     def totp_with_offset(self, offset):
-        return django_otp.oath.totp(self.bin_key, self.empty_form.step, 
-        	self.empty_form.t0, self.empty_form.digits, self.empty_form.drift + offset)
+        return django_otp.oath.totp(self.bin_key, self.empty_form.step,
+            self.empty_form.t0, self.empty_form.digits, self.empty_form.drift + offset)
 
     def test_offset_0(self):
         device_totp = self.totp_with_offset(0)
-        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None, 
-        	data={'token': device_totp})
+        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None,
+            data={'token': device_totp})
         self.assertTrue(self.form.is_valid())
 
     def test_offset_minus1(self):
         device_totp = self.totp_with_offset(-1)
-        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None, 
-        	data={'token': device_totp})
+        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None,
+            data={'token': device_totp})
         self.assertTrue(self.form.is_valid())
 
     def test_offset_plus1(self):
         device_totp = self.totp_with_offset(1)
-        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None, 
-        	data={'token': device_totp})
+        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None,
+            data={'token': device_totp})
         self.assertTrue(self.form.is_valid())
 
-    def test_offset_minus1(self):
+    def test_offset_minus2(self):
         device_totp = self.totp_with_offset(-2)
-        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None, 
-        	data={'token': device_totp})
+        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None,
+            data={'token': device_totp})
         self.assertFalse(self.form.is_valid())
 
     def test_offset_plus2(self):
         device_totp = self.totp_with_offset(2)
-        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None, 
-        	data={'token': device_totp})
+        self.form = TOTPDeviceForm(TOTPDeviceFormTest.key, None,
+            data={'token': device_totp})
         self.assertFalse(self.form.is_valid())
