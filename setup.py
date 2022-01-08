15e1852a700654ceafec410b2800e7c603f52228
Anurag Kumar <mailanu98@gmail.com>
2022-01-08 19:28:16 +0530

    updated classifiers
    added support for python 3.10

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Security',
         'Topic :: System :: Systems Administration :: Authentication/Directory',
     ],
