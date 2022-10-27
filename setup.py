c5f0b7e0282d2428240117e127de25b1e9b0ab0d
Josh <josh@joshthomas.dev>
2022-10-27 14:10:48 -0500

    Add Python 3.11 to CI, tox, and trove classifiers

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Security',
         'Topic :: System :: Systems Administration :: Authentication/Directory',
     ],
