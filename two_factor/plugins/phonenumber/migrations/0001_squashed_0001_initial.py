86f8c5bd89997d8be91944b2073cb985b9448517
Michal Dabski <mick88@users.noreply.github.com>
2023-03-20 15:18:08 +0000

    Merge squashed migrations

--- two_factor/plugins/phonenumber/migrations/0001_squashed_0001_initial.py
+++ two_factor/plugins/phonenumber/migrations/0001_squashed_0001_initial.py
@@ -9,6 +9,10 @@ import two_factor.plugins.phonenumber.models
 class Migration(migrations.Migration):
     replaces = [
         ('phonenumber', '0001_initial'),
+        ('two_factor', '0001_initial'), ('two_factor', '0002_auto_20150110_0810'),
+        ('two_factor', '0003_auto_20150817_1733'), ('two_factor', '0004_auto_20160205_1827'),
+        ('two_factor', '0005_auto_20160224_0450'), ('two_factor', '0006_phonedevice_key_default'),
+        ('two_factor', '0007_auto_20201201_1019'), ('two_factor', '0008_delete_phonedevice'),
     ]
 
     initial = True
