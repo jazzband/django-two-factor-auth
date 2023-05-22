3d685da331eff1695b99a5660789ce74754a80a0
alexsilva <geniofuturo@gmail.com>
2023-05-22 12:10:53 -0300

    Add setting 'TWO_FACTOR_EMAIL_APPCONFIG_NAME'.

--- two_factor/plugins/email/apps.py
+++ two_factor/plugins/email/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 from two_factor.plugins.registry import registry
 
 
 class TwoFactorEmailConfig(AppConfig):
-    name = 'two_factor.plugins.email'
+    # Allows you to resolve conflict between two apps called email.
+    name = getattr(settings, "TWO_FACTOR_EMAIL_APPCONFIG_NAME", 'two_factor.plugins.email')
     verbose_name = "Django Two Factor Authentication – Email Method"
 
     def ready(self):
